a6bd12cb546ee108341bdf50f92a39d1f96651ba
Jas Delgado <jas@jasdelgado.com>
2023-04-07 16:30:06 -0400

    signup form thing

---
+++ script.js
@@ -0,0 +1,35 @@
+// Get the form element
+const form = document.getElementById("waitlist-form");
+
+function sendData() {
+  const email = document.getElementById("email").value;
+  fetch(`https://vividly-backend.herokuapp.com/v0/waitlist`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
+  form.style.display = "none";
+  const successMessage = document.getElementById("signup-success");
+  successMessage.style.display = "block";
+}
+
+const button = document.getElementById("button");
+button.onclick = sendData;
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  sendData();
+});
